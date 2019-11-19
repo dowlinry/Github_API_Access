6c7bb2ae27b10c7979f4d57b6bbec56b92f3e5a7
dowlinry <dowlinry@tcd.ie>
2019-11-19 22:29:54 +0000

    Started to add in some code for database

--- web_server.py
+++ web_server.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, url_for
-web_server = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
+web_server = Flask(__name__)
+web_server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(web_server)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
 
 @web_server.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
+if  __name__ == '__main__':
     web_server.run(debug=True)
