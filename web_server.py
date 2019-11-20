8d44fb35a034440db90ae32eba4f6dc6ab3c3100
dowlinry <dowlinry@tcd.ie>
2019-11-20 12:53:08 +0000

    Added code to display database on webpage (temporary)

--- web_server.py
+++ web_server.py
@@ -8,10 +8,11 @@ import numpy as np
 import json
 
 web_server = Flask(__name__)
+df = pd.read_json('results.json')
 
 @web_server.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 if  __name__ == '__main__':
     web_server.run(debug=True)
