377a6822fa4527683f49efb2455802f277502444
dowlinry <dowlinry@tcd.ie>
2019-11-20 11:21:50 +0000

    Removed old database code, added new code for graph plotting

--- web_server.py
+++ web_server.py
@@ -1,17 +1,38 @@
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+
+import plotly
+import plotly.graph_objs as go
+
+import pandas as pd
+import numpy as np
+import json
 
 web_server = Flask(__name__)
-web_server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(web_server)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable = False)
+def create_plot():
+    
+
+    N = 40
+    x = np.linspace(0, 1, N)
+    y = np.random.randn(N)
+    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+
+
+    data = [
+        go.Bar(
+            x=df['x'], # assign x as the dataframe column 'x'
+            y=df['y']
+        )
+    ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
 
 @web_server.route('/')
 def index():
-    return render_template('index.html')
+    bar = create_plot()
+    return render_template('index.html', plot = bar)
 
 if  __name__ == '__main__':
     web_server.run(debug=True)
