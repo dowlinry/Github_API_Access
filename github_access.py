b33ae544b81dca1b65701cf9013f065c0c558504
dowlinry <dowlinry@tcd.ie>
2019-11-21 11:28:13 +0000

    Edited github_access.py to return a list of all known languages. Created a graph class for future use

--- github_access.py
+++ github_access.py
@@ -4,34 +4,30 @@ import pandas as pd
 import numpy as np
 
 
-g = Github("6102ec3c086ba008db6d251f6ec91baa09d06e75")
+g = Github("6de076fe0627f8df0c9a87b395fe299bea92b427")
 org = g.get_organization("amzn")
 members = org.get_members()
 
-df = pd.DataFrame(columns = ['ID', 'Popularity Score'])
-#df.insert(0,'69','420')
-#df.to_json(results.json)
-#print(df)
+df = pd.DataFrame(columns = ['ID', 'Popularity Score','Known Languages', 'Number of Known Languages'])
 
-#member_list = []
+member_count = 0
 
-count = 0
 for member in members:
- #   user = User(member.id)
     popularity_score = member.followers
     repos = member.get_repos()
+    languages = []
+    language_count = 0
     for repo in repos:
         if(member.id == repo.owner.id):
-            popularity_score = popularity_score + repo.stargazers_count 
-
-            
-
- #           if((repo.language not in user.languages) & (repo.language != None)):
- #               user.languages.append(repo.language) 
- #   user.add_popularity_score(popularity_score)
- #   member_list.append(user)
-    df.loc[count] = [member.id] + [popularity_score]
-    count += 1      
+            popularity_score = popularity_score + repo.stargazers_count
+            if((repo.language not in languages) & (repo.language is not None) & (repo.language != "Makefile")):
+                languages.append(repo.language) 
+                language_count += 1
+    if(language_count != 0):
+        known_languages = '|'.join(str(x) for x in languages)
+        df.loc[member_count] = [member.id] + [popularity_score] + [known_languages] + [language_count]
+        member_count += 1      
+    
 
 print(df)
 df.to_json('results.json')
